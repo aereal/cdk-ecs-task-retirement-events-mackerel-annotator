56d76e2b763ecb87bbcc9053d1e5a687a40335c2
aereal <aereal@aereal.org>
2020-01-30 16:43:10 +0900

    create dist directory

--- test/annotator-lambda.test.ts
+++ test/annotator-lambda.test.ts
@@ -1,9 +1,16 @@
+import { join, resolve } from "path";
+import { mkdirSync } from "fs";
 import { Stack } from "@aws-cdk/core";
 import { SynthUtils } from "@aws-cdk/assert";
 import { StringParameter } from "@aws-cdk/aws-ssm";
 import { EcsServiceEventsMackerelAnnotator } from "../src/resources/annotator-lambda";
 
 describe("EcsServiceEventsMackerelAnnotator", () => {
+  beforeAll(() => {
+    const lambdaPath = resolve(join(__dirname, "..", "dist", "annotator"));
+    mkdirSync(lambdaPath, { recursive: true });
+  });
+
   test("snapshot", () => {
     const stack = new Stack();
     new EcsServiceEventsMackerelAnnotator(stack, "Annotator", {
